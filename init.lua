171dbb793f4291f57c7035b463e3791d2cbfe6a2
Markus Engelbrecht <mgee@users.noreply.github.com>
2017-08-22 08:12:42 +0200

    Add custom menu with own icon and replace keyboard layers with karabiner config

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
+require("hs.ipc")
 local log = hs.logger.new('init', 'debug')
 
 ----------------------------------------------------------------------------------------------------
 -- Settings
 ----------------------------------------------------------------------------------------------------
-local hyper = {'shift', 'cmd', 'ctrl', 'alt'}
-local meh = {'shift', 'ctrl', 'alt'}
 
 hs.autoLaunch(true)
 hs.automaticallyCheckForUpdates(true)
 hs.consoleOnTop(true)
 hs.dockIcon(false)
-hs.menuIcon(true)
+hs.menuIcon(false)
 hs.uploadCrashData(false)
 
 expose = hs.expose.new(hs.window.filter.new():setDefaultFilter({allowTitles=1}),{
@@ -45,6 +44,43 @@ hs.grid.HINTS = {
 }
 
 ----------------------------------------------------------------------------------------------------
+-- Menu
+----------------------------------------------------------------------------------------------------
+
+function makeMenu()
+  return {
+       { title = 'Hammerspoon ' .. hs.processInfo.version, disabled = true },
+       { title = '-' },
+       { title = 'Reload', fn = function() hs.reload() end },
+       { title = 'Console...', fn = function() hs.openConsole() end },
+       { title = '-' },
+       { title = 'Input Source', disabled = true },
+       { title = 'Colehack', checked = hs.keycodes.currentLayout() == 'U.S.', fn = function() changeToColehack() end },
+       { title = 'German', checked = hs.keycodes.currentLayout() == 'German', fn = function() changeToGerman() end },
+       { title = '-' },
+       { title = 'Quit', fn = function() hs.application.get(hs.processInfo.processID):kill() end }
+  }
+end
+
+function selectKarabinerProfile(profile)
+  hs.execute("'/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile '" .. profile .. "'")
+end
+
+function changeToColehack()
+  hs.keycodes.setLayout('U.S.')
+  selectKarabinerProfile('Colehack')
+  menu:setMenu(makeMenu())
+end
+
+function changeToGerman()
+  hs.keycodes.setLayout('German')
+  selectKarabinerProfile('German')
+  menu:setMenu(makeMenu())
+end
+
+menu = hs.menubar.new():setIcon(hs.configdir .. '/statusicon.tiff'):setMenu(makeMenu())
+
+----------------------------------------------------------------------------------------------------
 -- Window Layout
 ----------------------------------------------------------------------------------------------------
 
@@ -56,70 +92,41 @@ function screenEast()
   return hs.screen.primaryScreen():toEast() or hs.screen.primaryScreen()
 end
 
+function maximize(win)
+  win:setFrame(win:screen():fullFrame(), 0.0)
+end
+
+function fillsGrid(win)
+  local gridSize = hs.grid.getGrid()
+  local winGrid = hs.grid.get(win)
+  return winGrid.x == 0 and winGrid.y == 0 and winGrid.w == gridSize.w and winGrid.h == gridSize.h
+end
+
 windowLayout = {
-  ['com.googlecode.iterm2'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.kapeli.dash'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['org.mozilla.firefox'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.apple.SafariTechnologyPreview'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.google.Chrome'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.apple.iTunes'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.agilebits.onepassword-osx'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.hicknhacksoftware.MacPass'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['org.vim.MacVim'] = (function(win)
-    win:moveToScreen(primaryScreen()):maximize()
-  end),
-
-  ['com.parallels.desktop.console'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.vmware.fusion'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.reederapp.rkit2.mac'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.fournova.Tower2'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
-  ['com.apple.dt.Xcode'] = (function(win)
-    win:moveToScreen(primaryScreen()):maximize()
-  end),
-
-  ['Alacritty'] = (function(win)
-    win:moveToScreen(screenEast()):maximize()
-  end),
-
+  ['com.googlecode.iterm2']             = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.kapeli.dash']                   = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['org.mozilla.firefox']               = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.apple.SafariTechnologyPreview'] = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.google.Chrome']                 = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.apple.iTunes']                  = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.agilebits.onepassword-osx']     = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.hicknhacksoftware.MacPass']     = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['org.vim.MacVim']                    = function(win) maximize(win:moveToScreen(primaryScreen())) end,
+  ['com.parallels.desktop.console']     = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.vmware.fusion']                 = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.reederapp.rkit2.mac']           = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.fournova.Tower2']               = function(win) maximize(win:moveToScreen(screenEast())) end,
+  ['com.apple.dt.Xcode']                = function(win) maximize(win:moveToScreen(primaryScreen())) end,
+  ['Alacritty']                         = function(win) maximize(win:moveToScreen(screenEast())) end,
 
   ['_'] = (function(win)
-    if not win:isFullScreen() then hs.grid.snap(win) end
+    if not win:isFullScreen() then
+      if fillsGrid(win) then
+        maximize(win)
+      else
+        hs.grid.snap(win)
+      end
+    end
   end),
 }
 
@@ -133,218 +140,45 @@ function handleWindowLayout(win)
   layout(win)
 end
 
+hs.window.filter.new():subscribe(hs.window.filter.windowCreated, handleWindowLayout)
+
+----------------------------------------------------------------------------------------------------
+-- Hotkey functions invoked from karabiner
+----------------------------------------------------------------------------------------------------
+
+function maximizeCurrentWindow()
+  maximize(hs.window.focusedWindow())
+end
+
+function moveCurrentWindowToNextScreen()
+  hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():next())
+end
+
+function moveCurrentWindowToLeftHalf()
+  local frame = hs.window.focusedWindow():screen():fullFrame()
+  hs.window.focusedWindow():setFrame({x = 0, y = 0, w = frame.w / 2.0, h = frame.h})
+end
+
+function moveCurrentWindowToRightHalf()
+  local frame = hs.window.focusedWindow():screen():fullFrame()
+  hs.window.focusedWindow():setFrame({x = frame.w / 2.0, y = 0, w = frame.w / 2.0, h = frame.h})
+end
+
 function applyLayoutToAllWindows()
   for _, win in pairs(hs.window.visibleWindows()) do handleWindowLayout(win) end
 end
 
-hs.window.filter.new():subscribe(hs.window.filter.windowCreated, handleWindowLayout)
+function lockScreen()
+  hs.caffeinate.lockScreen()
+end
+
+function toggleGrid()
+  hs.grid.toggleShow()
+end
 
 ----------------------------------------------------------------------------------------------------
--- Hotkey Bindings
+-- Startup Settings
 ----------------------------------------------------------------------------------------------------
-colorBlack = {red = 0.16, green = 0.16, blue = 0.16}
-colorBlue = {red = 0.27, green = 0.52, blue = 0.53}
-colorRed = {red = 0.8, green = 0.14, blue = 0.11}
-colorGreen = {red = 0.60, green = 0.59, blue = 0.10}
 
-function showBar(color, textColor)
-  if not barBackground then
-    local screenFrame = hs.screen.mainScreen():fullFrame()
-    local barFrame = {x = 0, y = screenFrame.h - 4, w = screenFrame.w, h = 4}
-
-    barBackground = hs.drawing.rectangle(barFrame)
-        :setStroke(false)
-        :setLevel(hs.drawing.windowLevels.modalPanel)
-  end
-  barBackground:setFillColor(color)
-  barBackground:show()
-end
-
-function hideBar()
-  if barBackground then barBackground:hide() end
-end
-
-function key(modifiers, k)
-  return function() hs.eventtap.keyStroke(modifiers, k, 0) end
-end
-
-function appByName(name)
-  return function() hs.application.launchOrFocus(name) end
-end
-
-function app(bundleID)
-  return function() hs.application.launchOrFocusByBundleID(bundleID) end
-end
-
-function mouseMove(offset)
-  return function()
-    hs.mouse.setRelativePosition(hs.geometry(hs.mouse.getRelativePosition()):move(offset))
-  end
-end
-
-function doubleClick()
-  local pt = hs.mouse.getAbsolutePosition()
-  local clickState = hs.eventtap.event.properties.mouseEventClickState
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseDown, pt):setProperty(clickState, 1):post()
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, pt):setProperty(clickState, 1):post()
-  hs.timer.usleep(1000)
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseDown, pt):setProperty(clickState, 2):post()
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, pt):setProperty(clickState, 2):post()
-end
-
-function scrollWheel(offset)
-  return function()
-    local win = hs.window.focusedWindow()
-    local frame = hs.geometry(win:frame())
-    if not hs.geometry(hs.mouse.getAbsolutePosition()):inside(frame) then
-      hs.mouse.setAbsolutePosition(frame.center)
-      win:focus()
-    end
-    hs.eventtap.scrollWheel(offset, {})
-  end
-end
-
-function createMomentaryLayer(layer, layout)
-  for _, entry in pairs(layout) do hs.hotkey.bind(layer, entry.k, entry.f, nil, entry.f) end
-end
-
-function createToggleLayer(color, layer, activationKey, layout)
-  local layerModal = hs.hotkey.modal.new(layer, activationKey)
-  function layerModal:entered() showBar(color) end
-  function layerModal:exited() hideBar() end
-  hs.fnutils.concat(layout, {{k = activationKey, exit = true}, {k = 'escape', exit = true}, {k = 'return', exit = true}})
-
-  for _, entry in pairs(layout) do
-    local func = function()
-      if entry.f then entry.f() end
-      if entry['exit'] then layerModal:exit() end
-    end
-    local mods = entry.m or {}
-    layerModal:bind(mods, entry.k, func, nil, func)
-  end
-end
-
-hs.hotkey.bind({'shift'}, 'delete', key({}, 'forwarddelete'), nil, key({}, 'forwarddelete'))
-
--- Standard Colemak Layer
--- ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
--- │  §  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  ─  │  =  │ BkSp  │
--- ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬────┤
--- │ Tab    │  Q  │  W  │  F  │  P  │  G  │  J  │  L  │  U  │  Y  │  ;  │  [  │  ]  │Entr│
--- ├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┐  │
--- │ Hyper    │  A  │  R  │  S  │  T  │  D  │  H  │  N  │  E  │  I  │  O  │  '  │  \  │  │
--- ├──────┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─────┴──┤
--- │LShift│  `  │  Z  │  X  │  C  │  V  │  B  │  K  │  M  │  ,  │  .  │  /  │ RShift/Esc │
--- ├──────┴─┬───┴──┬──┴─────┼─────┴─────┴─────┴─────┴─────┴─────┼─────┴──┬──┴───┬────────┤
--- │ LCtrl  │ Meh  │ LCmd   │ Space                             │ Meh    │ RCmd │ RCtrl  │
--- └────────┴──────┴────────┴───────────────────────────────────┴────────┴──────┴────────┘
-
--- Coding / Symbol Layer (momentary via Meh key)
--- ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
--- │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ BkSp  │
--- ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬────┤
--- │ Tab    │  !  │  @  │  {  │  }  │  |  │  \  │  7  │  8  │  9  │  *  │  ¨  │  ß  │Mous│
--- ├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┐eL│
--- │          │  #  │  $  │  (  │  )  │  &  │  -  │  4  │  5  │  6  │  +  │  '  │  .  │ay│
--- ├──────┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─────┴──┤
--- │      │  _  │  %  │  ^  │  [  │  ]  │  ~  │  =  │  1  │  2  │  3  │  0  │            │
--- ├──────┴─┬───┴──┬──┴─────┼─────┴─────┴─────┴─────┴─────┴─────┼─────┴──┬──┴───┬────────┤
--- │        │      │        │                                   │        │      │        │
--- └────────┴──────┴────────┴───────────────────────────────────┴────────┴──────┴────────┘
-
--- Application / OS Layer (momentary via Hyper key)
--- ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
--- │     │     │     │     │     │     │     │     │     │     │     │     │     │LockScr│
--- ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬────┤
--- │        │     │     │Files│ VM  │ Git │     │     │ Up  │     │     │     │     │Layo│
--- ├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┐ut│
--- │          │Stats│ News│ Pass│ Term│ Doc │     │Left │Down │Right│     │     │     │Wi│
--- ├──────┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─────┴──┤
--- │      │     │     │ IDE │     │ Vim │Brows│     │Music│     │     │     │            │
--- ├──────┴─┬───┴──┬──┴─────┼─────┴─────┴─────┴─────┴─────┴─────┼─────┴──┬──┴───┬────────┤
--- │        │      │        │ Expose                            │        │      │        │
--- └────────┴──────┴────────┴───────────────────────────────────┴────────┴──────┴────────┘
-
--- Mouse Layer (toggled via Meh+Space key)
--- ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
--- │     │     │     │     │     │     │     │     │     │     │     │     │     │       │
--- ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬────┤
--- │        │     │     │     │     │     │     │LClck│ Up  │RClck│     │     │     │Exit│
--- ├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┐  │
--- │          │     │     │     │     │     │     │Left │Down │Right│     │     │     │  │
--- ├──────┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─────┴──┤
--- │M 50px│     │     │     │     │     │     │     │     │     │     │     │            │
--- ├──────┴─┬───┴──┬──┴─────┼─────┴─────┴─────┴─────┴─────┴─────┼─────┴──┬──┴───┬────────┤
--- │M 100px │      │ M 2px  │ Exit                              │        │      │        │
--- └────────┴──────┴────────┴───────────────────────────────────┴────────┴──────┴────────┘
-
-createMomentaryLayer(hyper, {
-  {k = 'a', f = app('com.apple.ActivityMonitor')},
-  {k = 'b', f = app('com.apple.SafariTechnologyPreview')},
-  {k = 'c', f = app('com.google.Chrome')},
-  {k = 'd', f = app('com.kapeli.dash')},
-  {k = 'f', f = app('com.apple.Finder')},
-  {k = 'g', f = app('com.fournova.Tower2')},
-  {k = 'm', f = app('com.apple.iTunes')},
-  {k = 'p', f = app('com.vmware.fusion')},
-  {k = 'r', f = app('com.reederapp.rkit2.mac')},
-  {k = 's', f = app('com.agilebits.onepassword-osx')},
-  {k = 't', f = appByName('Alacritty')},
-  {k = 'v', f = app('org.vim.MacVim')},
-  {k = 'x', f = app('com.jetbrains.intellij')},
-
-  {k = 'n',      f = key({}, 'left')},
-  {k = 'u',      f = key({}, 'up')},
-  {k = 'e',      f = key({}, 'down')},
-  {k = 'i',      f = key({}, 'right')},
-  {k = 'up',     f = function() hs.window.focusedWindow():maximize() end},
-  {k = 'down',   f = function() hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():next()) end},
-  {k = 'left',   f = function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end},
-  {k = 'right',  f = function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1}) end},
-  {k = 'return', f = function() applyLayoutToAllWindows() end},
-  {k = 'delete', f = function() hs.caffeinate.lockScreen() end},
-  {k = 'space',  f = function() expose:toggleShow() end},
-  {k = '2',      f = function() hs.grid.toggleShow() end},
-  {k = 'tab',    f = function() switcher:next() end},
-  {k = '`',      f = function() switcher:previous() end},
-})
-
-createMomentaryLayer(meh, {
-  -- Left hand
-  {k = '1', f = key({}, 'f1')},       {k = '2', f = key({}, 'f2')},       {k = '3', f = key({}, 'f3')},       {k = '4', f = key({}, 'f4')},       {k = '5', f = key({}, 'f5')},
-  {k = 'q', f = key({'shift'}, '1')}, {k = 'w', f = key({'shift'}, '2')}, {k = 'f', f = key({'shift'}, '[')}, {k = 'p', f = key({'shift'}, ']')}, {k = 'g', f = key({'shift'}, '\\')},
-  {k = 'a', f = key({'shift'}, '3')}, {k = 'r', f = key({'shift'}, '4')}, {k = 's', f = key({'shift'}, '9')}, {k = 't', f = key({'shift'}, '0')}, {k = 'd', f = key({'shift'}, '7')},
-  {k = 'z', f = key({'shift'}, '5')}, {k = 'x', f = key({'shift'}, '6')}, {k = 'c', f = key({}, '[')},        {k = 'v', f = key({}, ']')},        {k = 'b', f = key({'shift'}, '`')},
-
-  -- Right hand
-  {k = '6', f = key({}, 'f6')}, {k = '7', f = key({}, 'f7')}, {k = '8', f = key({}, 'f8')}, {k = '9', f = key({}, 'f9')}, {k = '0', f = key({}, 'f10')},      {k = '-', f = key({}, 'f11')},    {k = '=', f = key({}, 'f12')},
-  {k = 'j', f = key({}, '\\')}, {k = 'l', f = key({}, '7')},  {k = 'u', f = key({}, '8')},  {k = 'y', f = key({}, '9')},  {k = ';', f = key({'shift'}, '8')}, {k = '[', f = key({'alt'}, 'u')}, {k = ']', f = key({'alt'}, 's')},
-  {k = 'h', f = key({}, '-')},  {k = 'n', f = key({}, '4')},  {k = 'e', f = key({}, '5')},  {k = 'i', f = key({}, '6')},  {k = 'o', f = key({'shift'}, '=')}, {k = '\\', f = key({}, '.')},
-  {k = 'k', f = key({}, '=')},  {k = 'm', f = key({}, '1')},  {k = ',', f = key({}, '2')},  {k = '.', f = key({}, '3')},  {k = '/', f = key({}, '0')},
-  {k = '`', f = key({'shift'}, '-')},
-})
-
-createToggleLayer(colorRed, meh, 'return', {
-  {k = 'space', exit = true},
-  {k = 'l', f = function() hs.eventtap.leftClick(hs.mouse.getAbsolutePosition()) end},
-  {k = 'y', f = function() hs.eventtap.rightClick(hs.mouse.getAbsolutePosition()) end},
-  {k = 'k', f = function() doubleClick() end},
-
-  {k = 'n', f = scrollWheel({5, 0})},
-  {k = 'u', f = scrollWheel({0, 5})},
-  {k = 'e', f = scrollWheel({0, -5})},
-  {k = 'i', f = scrollWheel({-5,  0})},
-  {k = 'n', f = mouseMove({x = -10, y = 0}),   m = {'shift'}},
-  {k = 'u', f = mouseMove({x = 0,   y = -10}), m = {'shift'}},
-  {k = 'e', f = mouseMove({x = 0,   y = 10}),  m = {'shift'}},
-  {k = 'i', f = mouseMove({x = 10,  y = 0}),   m = {'shift'}},
-  {k = 'n', f = mouseMove({x = -50, y = 0}),   m = {'ctrl'}},
-  {k = 'u', f = mouseMove({x = 0,   y = -50}), m = {'ctrl'}},
-  {k = 'e', f = mouseMove({x = 0,   y = 50}),  m = {'ctrl'}},
-  {k = 'i', f = mouseMove({x = 50,  y = 0}),   m = {'ctrl'}},
-  {k = 'n', f = mouseMove({x = -2,  y = 0}),   m = {'cmd'}},
-  {k = 'u', f = mouseMove({x = 0,   y = -2}),  m = {'cmd'}},
-  {k = 'e', f = mouseMove({x = 0,   y = 2}),   m = {'cmd'}},
-  {k = 'i', f = mouseMove({x = 2,   y = 0}),   m = {'cmd'}},
-})
+changeToColehack()
 
