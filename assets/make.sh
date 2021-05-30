57c2f341225fed0e6f472a51e8e56b096cbccf8d
Markus Engelbrecht <git@enbre.com>
2021-05-30 20:24:33 +0200

    Adjust icon script

--- assets/make.sh
+++ assets/make.sh
@@ -9,8 +9,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 makeIcon() {
   input="$1"
   output="$2"
-  svgexport "${input}" "${input}_32x32.png" 32:32
-  svgexport "${input}" "${input}_16x16.png" 16:16
+  npx svgexport "${input}" "${input}_32x32.png" 32:32
+  npx svgexport "${input}" "${input}_16x16.png" 16:16
   convert -units PixelsPerInch -density 144 "${input}_32x32.png" "${input}_32x32.tiff"
   convert -units PixelsPerInch -density 72  "${input}_16x16.png" "${input}_16x16.tiff"
   convert "${input}_32x32.tiff" "${input}_16x16.tiff" "${output}"
