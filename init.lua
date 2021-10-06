2e5c6f408f69cc4b02f9e901b22c44175e0fa4e0
Markus Engelbrecht <git@enbre.com>
2021-10-06 15:49:30 +0200

    Add postman binding

--- init.lua
+++ init.lua
@@ -134,6 +134,7 @@ hs.hotkey.bind(hyperModifier, "c", function() hs.application.launchOrFocusByBund
 hs.hotkey.bind(hyperModifier, "d", function() hs.application.launchOrFocusByBundleID("com.microsoft.VSCode") end)
 hs.hotkey.bind(hyperModifier, "f", function() hs.application.launchOrFocusByBundleID("com.apple.finder") end)
 hs.hotkey.bind(hyperModifier, "g", function() hs.application.launchOrFocusByBundleID("com.fournova.Tower3") end)
+hs.hotkey.bind(hyperModifier, "p", function() hs.application.launchOrFocusByBundleID("com.postmanlabs.mac") end)
 hs.hotkey.bind(hyperModifier, "r", function() hs.application.launchOrFocusByBundleID("com.reederapp.5.macOS") end)
 hs.hotkey.bind(hyperModifier, "s", function() hs.application.launchOrFocusByBundleID("com.markmcguill.strongbox.mac") end)
 hs.hotkey.bind(hyperModifier, "t", function() hs.application.launchOrFocusByBundleID("com.googlecode.iterm2") end)
